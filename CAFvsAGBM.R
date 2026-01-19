ee0ce2003d39a27d21a5539a6c9a558db415e789
KalhariManawasinghe <kmanawasinghe1@gmail.com>
2026-01-19 14:32:00 -0600

    Update CAFvsAGBM.R

--- CAFvsAGBM.R
+++ CAFvsAGBM.R
@@ -1,5 +1,5 @@
 library(readr)
-CAFAGBMF <- read_csv("Desktop/Publication/CAFvsAGBMF.csv")
+CAFAGBMF <- read_csv("Desktop/Publication/AGBM_photogrammtery.csv")
 
 # Assume your data is in a data frame called '14CAFAGBMF'
 # ConLAert Genotype to a factor (if it's not already)
